ed996589710a46bf0a9a5de5f67ca959eb2bffed
Emiliano Martinez <emilianomartinez1933@gmail.com>
2022-04-21 20:18:10 -0300

    cambios en js

--- master_js/main.js
+++ master_js/main.js
@@ -1,45 +1,100 @@
+const formulario = document.getElementById("formulario");
 
-alert("Hola, Bienvenido a Rocket Comis!!");
+const inputs = document.querySelectorAll("#formulario input");
 
-let ingreseNombre = prompt(`Como te llamas?`);
+const texto = document.querySelector("#formulario textarea");
 
-
-if( ingreseNombre != ""){
-   alert(`Hola ` + ingreseNombre + ` un gusto :)`);
-}else if (ingreseNombre == ""){
-   alert(`No me dijiste tu nombre...`);
-   ingreseNombre = prompt(`Como te llamas?`);
-   alert(`Hola ` + ingreseNombre + ` un gusto :)`);
+const expresiones = {
+   apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+   nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+   email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+   telefono: /^\d{7,14}$/,
+   mensaje: /^[a-zA-ZÀ-ÿ\s]{1,40}$/
 }
 
-let compraTotal = 0;
-
-let producto = prompt(`Que te gustaria comprar...? mangas, comics o novelas?`);
-
-let precio = parseFloat(prompt(`Ok...Ingrese su precio por favor`));
-
-let cantidad = parseInt(prompt(`Muy bien!! Que cantidad?`));
-
-let compra = alert(`Su compra es de $`+ (precio*cantidad));
-
-let sugerencia = prompt(`Quiere seguir comprando?`)
-
-if (sugerencia == "si") {
-   
-let producto2 = prompt(`Que te gustaria comprar mangas, comics o novelas?`);
-
-let precio2 = parseFloat(prompt(`Ok...Ingrese su precio por favor`));
-
-let cantidad2 = parseInt(prompt(`Muy bien!! Que cantidad?`));
-
-let compra2 = alert(`Su compra es de $`+ (precio2*cantidad2));
-   
-} else if (sugerencia == "no") {
-   alert("gracias por su compra :)");
+const campos = {
+   nombre: false,
+   apellido: false,
+   email: false,
+   telefono: false,
+   mensaje: false
 }
 
-compraTotal =alert( "El total es $" + (compra+compra2));
+const validarFormulario = (e) => {
+switch (e.target.name) {
+   case "nombre":
+     formCorrecto (expresiones.nombre, e.target, "nombre");
+   break;
+   case "apellido":
+      formCorrecto (expresiones.apellido, e.target, "apellido");
+   break;
+   case "email":
+      formCorrecto (expresiones.email, e.target, "email");
+   break;
+   case "telefono":
+      formCorrecto (expresiones.telefono, e.target, "telefono");
+   break;
+   case "mensaje":
+      formCorrecto2 (expresiones.mensaje, e.target, "mensaje");
+   break;
+   }
+}
 
+const formCorrecto = (expresion, input, campo) => {
+   if(expresion.test(input.value)){
+      document.getElementById(`grupo__${campo}`).classList.remove("form__grupo-mal");
+      document.getElementById(`grupo__${campo}`).classList.add("form__grupo-bien");
+      document.querySelector(`#grupo__${campo} .form__error-input`).classList.remove("form__error-input-activo");
+      campos[campo] = true;
+   } else {
+      document.getElementById(`grupo__${campo}`).classList.add("form__grupo-mal");
+      document.getElementById(`grupo__${campo}`).classList.remove("form__grupo-bien");
+      document.querySelector(`#grupo__${campo} .form__error-input`).classList.add("form__error-input-activo");
+      campos[campo] = false;
+   }
+}
+
+const formCorrecto2 = (expresion, textarea, campo) => {
+   if(expresion.test(textarea.value)){
+      document.getElementById(`grupo__${campo}`).classList.remove("form__grupo-mal");
+      document.getElementById(`grupo__${campo}`).classList.add("form__grupo-bien");
+      document.querySelector(`#grupo__${campo} .form__error-input`).classList.remove("form__error-input-activo");
+      campos[campo] = true;
+   } else {
+      document.getElementById(`grupo__${campo}`).classList.add("form__grupo-mal");
+      document.getElementById(`grupo__${campo}`).classList.remove("form__grupo-bien");
+      document.querySelector(`#grupo__${campo} .form__error-input`).classList.add("form__error-input-activo");
+      campos[campo] = false;
+   }
+}
+
+inputs.forEach((input) => {
+   input.addEventListener('keyup', validarFormulario);
+   input.addEventListener('blur', validarFormulario);
+});
+
+texto.forEach((textarea) => {
+   textarea.addEventListener('keyup', validarFormulario);
+   textarea.addEventListener('blur', validarFormulario);
+});
+
+
+formulario.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   
+   const terminos = document.getElementById("terminos");
+	if(campos.nombre && campos.apellido && campos.email && campos.telefono && campos.mensaje && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById("form__exito").classList.add("form__exito-activo");
+		setTimeout(() => {
+			document.getElementById("form__exito").classList.remove("form__exito-activo");
+		}, 5000); 
+	} else {
+		document.getElementById("form_error-mensaje").classList.add("form_error-mensaje-activo");
+	}
+});
 
 
 
